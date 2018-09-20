0d7ea14eef8cc2e26219b226a038f16b868c07c2
Bobby Morehead <bobby.morehead@outlook.com>
2018-09-19 21:58:56 -0400

    Update 1

--- MonteCarloMethod(Pi)/Program.cs
+++ MonteCarloMethod(Pi)/Program.cs
@@ -10,6 +10,30 @@ namespace MonteCarloMethod_Pi_
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("How many times will you run this function?");
+            var timesToRunProgram = int.Parse(Console.ReadLine();
+
+            Random random = new Random();
+            double x = NextDouble(random[timesToRunProgram]);
+            double y = r.NextDouble[timesToRunProgram];
         }
-    }
+        struct Coords
+        {
+            double x;
+            double y;
+
+            public Coords(double x, double y)
+            {
+                double newx = x ;
+                double newy = y;
+            }
+        }
+
+        public double HyponteneuseOfTriangle(double newx, double newy)
+        {
+            double hypoteneuseValue = Math.Sqrt((newx * newx) + (newy * newy));
+
+        }
+    }  
 }
