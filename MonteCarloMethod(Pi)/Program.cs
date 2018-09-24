09f9440bd33b68a5772145efe33dc7fc833bc25c
Bobby Morehead <bobby.morehead@outlook.com>
2018-09-23 21:42:34 -0400

    Random Numbers

--- MonteCarloMethod(Pi)/Program.cs
+++ MonteCarloMethod(Pi)/Program.cs
@@ -12,28 +12,38 @@ namespace MonteCarloMethod_Pi_
         {
 
             Console.WriteLine("How many times will you run this function?");
-            var timesToRunProgram = int.Parse(Console.ReadLine();
+            int iterations = int.Parse(Console.ReadLine());
 
+            var x = new double[iterations];
+            var y = new double[iterations];
             Random random = new Random();
-            double x = NextDouble(random[timesToRunProgram]);
-            double y = r.NextDouble[timesToRunProgram];
-        }
-        struct Coords
-        {
-            double x;
-            double y;
 
-            public Coords(double x, double y)
+            for (int i = 0; i < iterations; i++)
             {
-                double newx = x ;
-                double newy = y;
+                x[i] = random.NextDouble();
+                y[i] = random.NextDouble();
             }
-        }
+            for (int i = 0; i < iterations; i++)
+            {
+                Console.WriteLine("x: {0}, y: {1}", x[i], y[i]);
+            }
 
-        public double HyponteneuseOfTriangle(double newx, double newy)
-        {
-            double hypoteneuseValue = Math.Sqrt((newx * newx) + (newy * newy));
-
         }
-    }  
+       //public double HyponteneuseOfTriangle(double x,double y)
+       //{
+       //     double output = Math.Sqrt((x * x) + (y * y));
+       //     if (output <= 1)
+       //     {
+       //         output.
+       //     }
+       //  
+       //}
+    }  ////
 }
+//Answers
+//1. To account for the rest of the circle.
+//2. With increasing parameters you will get closer to Pi.
+//3. If its truly random no.
+//4.
+//5. Monte Carlo method used to randomly figure out presidents terms.
+
